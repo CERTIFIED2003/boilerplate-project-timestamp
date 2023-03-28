1514f34303cd9749245c709a532e4a689b0ddc39
CERTIFIED2003 <111045472+CERTIFIED2003@users.noreply.github.com>
2023-03-28 13:30:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 // Timestamp API endpoint
-app.get("/api/timestamp/:date", (req, res) => {
+app.get("/api/:date", (req, res) => {
   let response = {
     unix: String
   };
